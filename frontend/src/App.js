c5620a104c6c727b5665196dae7180f95dbc02a3
lee <leedong1848@gmail.com>
2022-09-17 22:08:22 +0900

    Add nginx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,46 +1,44 @@
 import { useState } from 'react';
 import './App.css';
 
-function MyTable(props) {
-  return (
-  props.list == undefined || props.list == [] ?
-  <h1>load data</h1> :
-  <table>
-    <tr>
-      <th>id</th>
-      <th>memo</th>
-    </tr>
-    {
-    props.list.map((e) => {
-      <tr>
-        <td>e.id</td>
-        <td>e.memo</td>
-      </tr>
-    })
-    }
-</table>);
-}
-
 function App() {
 
   const [data, setData] = useState([]);
-  fetch("/api/memo")
-  .then(res => res.json())
-  .then(res => setData(res));
+  const [isRerender, setIsRerender] = useState(true);
+  if (isRerender) {
+    fetch("/api/memo")
+    .then(res => res.json())
+    .then(res => setData(res));
+    setIsRerender(false);
+  }
+
+  function postMemo(e) {
+    fetch("/api/memo", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        data: e.target.elements.data.value
+      })
+    }).then(() => setIsRerender(true))
+    return false;
+  }
 
   return (
     <div className="App">
-        <form method="post" action="/api/memo">
-          <label for="data"> your memo </label>
-          <input
-            type="text"
-            id="data"
-            name="data"
-            placeholder='type your memo'>
-          </input>
-          <button> submit </button>
-        </form>
-        <MyTable list={data}/>
+      <form method='GET' action="/" onSubmit={postMemo}>
+        <label for="data"> your memo </label>
+        <input
+          type="text"
+          id="data"
+          name="data"
+          placeholder='type your memo'/>
+          <button>submit</button>
+      </form>
+      <ul>
+        {data.map((num) => <li key={num.id}>{num.data}</li>)}
+      </ul>
     </div>
   );
 }
